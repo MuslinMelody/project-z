aeac999d13e863571638ed97858a4b012632365a
MuslinMelody <60828599+MuslinMelody@users.noreply.github.com>
2020-02-08 16:36:59 -0500

    Add files via upload

---
+++ papaoutai2.js
@@ -0,0 +1,360 @@
+function loadSong() {
+demoSong = `
+max 36
+min 21
+beat .12
+ jmp ahdites
+ done
+jmp a
+jmp b
+jmp c
+jmp outes
+jmp outai1
+jmp outes
+jmp outai2
+jmp quoi
+jmp serons
+jmp ahdites
+jmp monsieur
+jmp outes
+jmp outai1
+jmp outes
+jmp outai2
+jmp c
+jmp c
+jmp outes
+jmp outai1
+jmp outes
+jmp outai2
+done
+a:
+newscreen
+4 no
+2 28 Di
+1 28 tes-
+2 28 moi 
+1 26 d'où 
+2 28 il 
+4 26 vient,<br>
+2 24 En
+2 26 fin 
+2 28 je 
+1 26 sau
+2 28 rai 
+1 26 où 
+2 28 je 
+4 26 vais<br>
+2 24 Ma
+2 26 man 
+2 28 dit 
+1 26 que 
+2 28 lors
+1 26 qu'on 
+2 28 cherche 
+4 26 bien,<br>
+2 24 On 
+2 26 fi
+2 28 nit 
+1 26 tou
+2 28 jours 
+1 26 par 
+2 28 trou
+4 26 ver<br>
+ret
+b:
+newscreen
+2 24 Elle 
+2 26 dit 
+2 28 qu'il 
+1 28 n'est 
+2 28 ja
+1 28 mais 
+2 28 très 
+4 29 loin<br>
+2 29 Qu'il 
+2 29 part 
+2 28 très 
+1 28 sou
+2 28 vent 
+1 28 tra
+2 28 vai
+4 29 ller<br>
+2 29 Ma
+2 29 man 
+2 28 dit 
+1 28 «tra
+1 28 vai
+2 28 ller 
+2 28 c'est 
+4 29 bien»<br>
+2 29 Bien 
+2 29 mieux 
+2 28 qu'êt
+1 28 re  
+2 28 mal 
+2 28 a
+1 28 com
+2 31 pag
+2 31 né. 
+2 33 Pas 
+4 33 vrai?
+ret
+c:
+newscreen
+1 33 Où 
+2 34 est 
+1 31 ton 
+2 31 pa
+6 31 pa? 
+2 33 Dis-
+1 33 moi où 
+2 34 est 
+1 31 ton 
+2 31 pa
+6 31 pa<br>
+3 33 Sans 
+3 33 même 
+1 31 de
+1 31 voir 
+1 31 lu
+1 31 i 
+1 31 par
+1 36 l
+1 34 e
+2 33 r<br>
+2 31 Il 
+2 33 sait 
+1 33 ce 
+2 33 qui 
+1 31 ne 
+2 31 va 
+6 31 pas,<br>
+3 33 ah 
+2 33 sa
+1 31 cré<br>
+newscreen
+2 31 pa
+2 31 pa
+4 31 a,<br>
+2 33 Dis-
+1 33 moi où 
+2 33 es-
+1 31 tu 
+2 31 ca
+4 34 ché<br>
+2 28 Ça 
+4 29 doit, 
+1 33 faire 
+2 33 au 
+2 31 moins 
+1 34 mille 
+2 34 fois 
+2 33 que 
+4 31 j'ai<br>
+2 33 comp
+1 33 té 
+2 31 mes 
+4 31 doigts. 
+6 34 Hé!
+ret
+outes:
+newscreen
+2 31 Où 
+3 31 t'es, 
+2 31 pa
+1 31 pa 
+2 34 où 
+6 36 t'es?<br>
+2 31 Où 
+3 31 t'es, 
+2 31 pa
+1 31 pa 
+2 34 où 
+6 33 t'es?<br>
+2 31 Où 
+3 31 t'es, 
+2 31 pa
+1 31 pa 
+2 34 où 
+2 36 t'es?<br>
+ret
+outai1:
+2 34 où 
+2 34 t'es 
+2 33 où 
+2 33 t'es 
+1 31 où 
+2 31 pa
+1 31 pa 
+2 31 où 
+6 31 t'es
+ret
+outai2:
+2 34 où 
+2 34 t'es 
+2 33 où 
+2 33 t'es 
+1 31 où 
+2 31 pa
+1 31 pa 
+2 31 où 
+6 34 t'es
+ret
+quoi:
+newscreen
+6 28 Quoi, 
+1 28 qu'on 
+1 28 y 
+2 28 croit 
+2 28 ou 
+2 28 pas<br>
+1 21 Y 
+1 21 aura 
+2 28 bien 
+2 28 un 
+2 28 jour 
+1 28 où 
+1 28 on y 
+2 25 croi
+1 25 ra 
+4 24 plus<br>
+
+1 21 Un 
+2 28 jour 
+1 27 où 
+2 28 l'autre 
+1 27 on 
+1 27 se
+1 28 ra 
+2 27 tous 
+2 28 pa
+2 28 pa<br>
+1 21 et 
+1 21 d'un 
+2 28 jour 
+1 27 à 
+2 28 l'autre 
+1 21 on 
+1 24 au
+1 26 ra 
+2 24 dis
+1 22 pa
+2 21 ru
+ret
+serons:
+newscreen
+1 28 Ser
+2 28 ons-
+1 26 nous 
+2 26 dé
+2 28 te
+3 26 sta
+4 26 ble?<br>
+1 28 Ser
+2 28 ons-
+1 26 nous 
+2 26 ad
+2 28 mi
+3 26 ra
+4 26 ble?<br>
+2 26 Des 
+1 28 gé
+1 28 ni
+2 28 teurs 
+1 26 ou 
+2 28 des 
+1 2b gé
+2 28 nies?<br>
+2 28 Dites-
+2 28 nous 
+2 2b qui 
+1 28 donne 
+1 2b nai
+1 28 ssance 
+1 28 aux 
+1 28 ir
+1 2b re
+2 28 spon
+2 2b sa
+1 31 b
+2 31 les
+ret
+ahdites:
+newscreen
+4 34 Ah 
+2 33 dites-
+2 31 nous 
+4 31 qui 
+4 31 tiens<br>
+2 31 Tout 
+1 31 le 
+2 31 monde 
+1 31 sait 
+1 31 com
+1 31 ment 
+1 31 on 
+2 31 fait 
+1 31 des 
+1 34 bé
+2 34 bés,<br>
+2 2b Mais 
+1 31 per
+2 31 sonne 
+1 31 sait 
+1 31 com
+1 31 ment 
+2 31 on 
+1 31 fait 
+1 31 des 
+1 31 pa
+4 31 pas
+ret
+monsieur:
+newscreen
+2 31 Mon
+2 31 sieur 
+1 31 Je-
+1 31 sais-
+2 31 tout 
+1 31 en 
+1 31 aur
+2 34 ait 
+1 31 héri
+3 34 té, 
+2 30 c'est 
+4 31 ça<br>
+2 31 Faut 
+1 31 le 
+2 31 sucer 
+1 31 de 
+2 31 son 
+2 34 pouce 
+2 33 ou 
+4 31 quoi?<br>
+2 31 Dites-
+1 31 nous 
+2 31 où 
+1 31 c'est 
+2 31 ca
+4 34 ché, 
+2 27 ça 
+4 29 doit<br>
+2 33 Faire 
+1 31 au 
+1 31 moins 
+2 34 mille 
+2 34 fois 
+1 33 qu'on 
+4 31 a 
+2 33 bou
+1 33 ffé 
+2 31 nos 
+4 31 doigts<br>
+4 34 Hé!
+ret
+`;
+}
+
+
+
+//    /vjʊ/
